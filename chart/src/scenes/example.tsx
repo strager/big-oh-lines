e5a603f96e636d81068be3bc1dfbcfdb83ff056d
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-04-14 23:35:25 -0700

    refactor(chart): animate end= instead of animating points=
    Animating end= might be more flexible.

--- chart/src/scenes/example.tsx
+++ chart/src/scenes/example.tsx
@@ -1,11 +1,12 @@
+import data from '../../../data.json';
 import type {ThreadGenerator} from '@motion-canvas/core/lib/threading';
-import {createSignal} from '@motion-canvas/core/lib/signals';
 import {Circle, Line} from '@motion-canvas/2d/lib/components';
 import {all} from '@motion-canvas/core/lib/flow';
 import {createRef} from '@motion-canvas/core/lib/utils';
+import {createSignal} from '@motion-canvas/core/lib/signals';
 import {linear} from '@motion-canvas/core/lib/tweening';
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import data from '../../../data.json';
+import {waitFor} from '@motion-canvas/core/lib/flow';
 
 let serieses = new Map();
 for (let sample of data) {
@@ -26,17 +27,44 @@ for (let seriesSamples of serieses.values()) {
 }
 
 export default makeScene2D(function* (view) {
-    let maxX = 0;
+    function getX(sample) {
+        return sample.text_bytes / 10;
+    }
 
-    let xS = createSignal(0);
-
-    for (let [key, seriesSamples] of serieses.entries()) {
-        if (!key.endsWith(',near beginning,equal')) {
+    let seriesNames = [];
+    let xs = new Set();
+    for (let [seriesName, seriesSamples] of serieses.entries()) {
+        if (!seriesName.endsWith(',near beginning,realisticish')) {
             continue;
         }
-        let points = seriesSamples.map(sample => [sample.text_lines, -sample.comparisons]);
-        maxX = Math.max(points.length, maxX);
-        let pointsS = createSignal(() => points.slice(0, xS()));
+        seriesNames.push(seriesName);
+        for (let sample of seriesSamples) {
+            xs.add(getX(sample));
+        }
+    }
+    xs = [...xs].sort((a, b) => {
+        if (a < b) return -1;
+        if (a > b) return +1;
+        return 0;
+    });
+
+    let iS = createSignal(0);
+    for (let seriesName of seriesNames) {
+        let seriesSamples = serieses.get(seriesName);
+        let points = seriesSamples.map((sample) => [getX(sample), -sample.comparisons]);
+
+        let cumulativeDistances = [0];
+        for (let i = 0; i < xs.length - 1; ++i) {
+            let p0 = points[i];
+            let p1 = points[i + 1];
+            let dx = p0[0] - p1[0];
+            let dy = p0[1] - p1[1];
+            let distance = Math.sqrt(dx*dx + dy*dy);
+            cumulativeDistances.push(distance + cumulativeDistances.at(-1));
+        }
+        let totalDistance = cumulativeDistances.at(-1);
+
+        let endS = createSignal(() => cumulativeDistances[iS()] / totalDistance);
         let line = createRef();
         view.add(
             <Line
@@ -44,14 +72,14 @@ export default makeScene2D(function* (view) {
                 x={0}
                 y={0}
                 lineWidth={2}
-                end={1}
-                points={pointsS}
+                end={endS}
+                points={points}
                 stroke="#e13238"
             />,
         );
     }
-
-    for (; xS() < maxX; xS(xS() + 1)) {
-        yield null;
+    for (; iS() < xs.length - 1; iS(iS() + 1)) {
+        // FIXME(strager): This still causes jitter.
+        yield *waitFor((xs[iS() + 1] - xs[iS()]) / 400);
     }
 });
