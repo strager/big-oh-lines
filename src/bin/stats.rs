c15176819c2b1aa12f35eb245362640a38e65a66
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-04-14 18:14:37 -0700

    feat: generate random input text

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -2,11 +2,20 @@ use big_oh_lines::*;
 use bol_base::*;
 
 pub fn main() {
+    test("realisticish", &generate_realisticish_text(1000), 9);
+    test(
+        "equal length lines",
+        &generate_equal_length_line_text(10, 30),
+        9,
+    );
+}
+
+fn test(name: &str, text: &[u8], offset: usize) {
+    eprintln!("=== {} ===", name);
     let imps: Vec<Implementation> = load_implementations();
     for imp in imps {
-        let text: &[u8] = b"hello\nworld\n\nlast line";
         let mut bol: BOL = imp.create(text);
-        assert_eq!(bol.offset_to_line(b"hello\nwor".len()), 1);
+        bol.offset_to_line(9);
         let stats: BOLStats = bol.stats();
         eprintln!("{} {:?}", imp, stats);
     }
