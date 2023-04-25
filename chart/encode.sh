70dcc80d85dbbbfef9e2e9670b45d887fcca5b9f
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-04-25 15:45:43 -0700

    refactor(chart): format encode script for readability

--- chart/encode.sh
+++ chart/encode.sh
@@ -6,7 +6,15 @@ set -u
 cd "$(dirname "${0}")"
 cd output/project/
 for dir in "${@}"; do
-  ffmpeg -y -framerate 60 -pattern_type glob -i "${dir}/*.webp" -pix_fmt yuv420p -vcodec libx264 -qp 1 "${dir}.mp4"
+  ffmpeg \
+      -y \
+      -framerate 60 \
+      -pattern_type glob \
+      -i "${dir}/*.webp" \
+      -pix_fmt yuv420p \
+      -vcodec libx264 \
+      -qp 1 \
+      "${dir}.mp4"
   convert "$(printf '%s\n' "${dir}/"*.webp | sort | head -n1)" "${dir}.first.png"
   convert "$(printf '%s\n' "${dir}/"*.webp | sort | tail -n1)" "${dir}.last.png"
 done
