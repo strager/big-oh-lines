bbef3b058f85e838bf56ef34b33ace0e6869aabf
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-04-24 21:38:06 -0700

    fix(chart): generate .png files instead of .webp for better PowerPoint compat

--- chart/encode.sh
+++ chart/encode.sh
@@ -7,6 +7,6 @@ cd "$(dirname "${0}")"
 cd output/project/
 for dir in "${@}"; do
   ffmpeg -y -framerate 60 -pattern_type glob -i "${dir}/*.webp" -pix_fmt yuv420p -vcodec libx264 -qp 1 "${dir}.mp4"
-  cp "$(printf '%s\n' "${dir}/"*.webp | sort | head -n1)" "${dir}.first.webp"
-  cp "$(printf '%s\n' "${dir}/"*.webp | sort | tail -n1)" "${dir}.last.webp"
+  convert "$(printf '%s\n' "${dir}/"*.webp | sort | head -n1)" "${dir}.first.png"
+  convert "$(printf '%s\n' "${dir}/"*.webp | sort | tail -n1)" "${dir}.last.png"
 done
